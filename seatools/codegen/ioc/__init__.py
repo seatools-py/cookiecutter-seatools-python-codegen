53e816066ce33e18d9781a285689d7180e594bd5
dragons96 <jinqunlong@black-unique.com>
2024-11-08 17:01:04 +0800

    init

---
+++ seatools/codegen/ioc/__init__.py
@@ -0,0 +1,7 @@
+from .cmd import generate_cmd
+from .django import generate_django
+from .fastapi import generate_fastapi
+from .flask import generate_flask
+from .grpc import generate_grpc
+from .scrapy import generate_scrapy, generate_scrapy_spider
+from .task import generate_task
