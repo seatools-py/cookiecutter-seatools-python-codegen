5aae37e991f6da6db7a40b11601d29544b333cc1
dragons96 <jinqunlong@black-unique.com>
2024-11-26 15:36:59 +0800

    release: 1.0.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://gitee.com/dragons96/cookiecutter-seatools-python-codegen',
-    version='1.0.5',
+    version='1.0.6',
     zip_safe=False,
     entry_points={
         'console_scripts': [
